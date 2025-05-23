ff59669831d4625bf612f5168a89f1bef54ce4ce
AgataPrzykaza <agataprzykaza89@gmail.com>
2025-05-23 09:59:19 +0200

    gitignore

---
+++ CoreDataExercise/View/Book/BookListView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  CoreDataExercise
+//
+//  Created by Agata Przykaza on 22/05/2025.
+//
+
+import SwiftUI
+
+struct BookListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BookListView()
+}
